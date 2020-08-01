0dcab742fde975b58826dc4c7c9033bfb46334eb
shelryn <65678107+shelryn@users.noreply.github.com>
2020-08-01 13:43:18 +0300

    Add files via upload

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,50 @@
-// When the user clicks on <div>, open the popup
-function myFunction() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
+$(document).ready(function(){
+    //Скрыть PopUp при загрузке страницы
+    HideContact()
+    PopUpHide();
+    PopUpHide2();
+    PopUpHide3();
+    PopUpHide4();
+}
+);
+//Функция отображения PopUp
+function PopUpShow(){
+    $("#popup1").show();
+}
+//Функция скрытия PopUp
+function PopUpHide(){
+    $("#popup1").hide();
+}
+//Функция отображения PopUp
+function PopUpShow2(){
+    $("#popup2").show();
+}
+//Функция скрытия PopUp
+function PopUpHide2(){
+  $("#popup2").hide();
+}
+//Функция отображения PopUp
+function PopUpShow3(){
+    $("#popup3").show();
+}
+//Функция скрытия PopUp
+function PopUpHide3(){
+  $("#popup3").hide();
+}
+//Функция отображения PopUp
+function PopUpShow4(){
+    $("#popup4").show();
+}
+//Функция скрытия PopUp
+function PopUpHide4(){
+  $("#popup4").hide();
+}
+//Функция отображения PopUp
+function ShowContact(){
+    $("#contact").show();
+    setTimeout(HideContact, 10000);
+}
+//Функция скрытия PopUp
+function HideContact(){
+  $("#contact").hide();
 }
